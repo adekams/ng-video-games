fabae1ad319d7e40d0dfe1ef3efa4ac63f36a410
adekams <kifademi@gmail.com>
2022-01-13 18:05:15 +0100

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { SuiModule } from '@richardlt/ng2-semantic-ui';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchbarComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
