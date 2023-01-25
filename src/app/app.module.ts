9c6fb9cc6012a52efa63bc3186947725899a8a52
Adekams <kifademi@gmail.com>
2023-01-26 00:00:16 +0100

    interceptors update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuiModule } from '@richardlt/ng2-semantic-ui';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpService } from './services/http.service';
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
 
 @NgModule({
   declarations: [AppComponent, SearchbarComponent, HomeComponent],
@@ -19,7 +20,19 @@ import { HttpService } from './services/http.service';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [HttpService],
+  providers: [
+    HttpService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
