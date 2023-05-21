3d5e5e2fb7fae74e21e7bfb41595e7bcdcc029e9
Adekams <kifademi@gmail.com>
2023-05-21 18:45:41 +0100

    updating search

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { APIResponse, Game } from 'src/app/models/app-filter/app-filter';
 import { HttpService } from '../../services/http.service';
 
@@ -8,21 +10,25 @@ import { HttpService } from '../../services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  selectedOption: any = '';
-
-  options: any = [
-    'Name',
-    'Released',
-    'Added',
-    'Created',
-    'Updated',
-    'Rating',
-    'Metacritic',
+export class HomeComponent implements OnInit, OnDestroy {
+  unsubscribe$: Subject<boolean> = new Subject();
+  sortBy: any;
+  pending: boolean = false;
+  sortOptions: any = [
+    {
+      name: 'Name',
+      value: 'name',
+    },
+    { name: 'Released', value: '-released' },
+    { name: 'Added', value: '-added' },
+    { name: 'Created', value: '-created' },
+    { name: 'Updated', value: '-uploaded' },
+    { name: 'Rating', value: '-rating' },
+    { name: 'Metacritic', value: 'metacritic' },
   ];
-  namedOptions = this.options.map((name) => ({ name }));
+  // namedOptions = this.options.map((name) => ({ name }));
   games: Array<Game> = [];
-  sort: string;
+  // sort: string;
   noImg: string =
     'https://res.cloudinary.com/adenike/image/upload/v1642002314/no-image_iah8ux.png';
   constructor(
@@ -31,7 +37,7 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.options = this.namedOptions;
+    // this.options = this.namedOptions;
     this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
@@ -41,12 +47,29 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  consoleSort(item) {
+    console.log(item);
+  }
   searchGames(sort: string, search?: string): void {
+    this.pending = true;
     this.http
       .getGames(sort, search)
-      .subscribe((gameList: APIResponse<Game>) => {
-        this.games = gameList.results;
-        console.log(gameList);
-      });
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (res: APIResponse<Game>) => {
+          this.pending = false;
+          this.games = res.results;
+          console.log(sort);
+        },
+        (error) => {
+          this.pending = false;
+          console.log(error);
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.unsubscribe();
   }
 }
