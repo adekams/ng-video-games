afb5c34b0f54ce1e25c7284005a2839195cf4171
Adekams <kifademi@gmail.com>
2023-11-19 22:32:59 +0100

    slider component

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+  @Input() images: Array<any>;
+  constructor() {
+    setTimeout(() => {
+      console.log('passsed ', this.images)
+    }, 5000);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
