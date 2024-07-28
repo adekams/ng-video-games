175285a3bc5b60cfa5e50ff980c5f5d726919c2e
Adekams <kifademi@gmail.com>
2024-07-29 00:22:07 +0100

    auto search

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-searchbar',
@@ -13,6 +14,16 @@ export class SearchbarComponent implements OnInit {
   constructor(private _router: Router, private fb: FormBuilder) {
     this.searchForm = fb.group({
       searchQuery: ['', Validators.required],
+    });
+
+    this.searchForm.get("searchQuery").valueChanges
+    .pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    )
+    .subscribe((i) => {
+      console.log('debouncing ', i)
+      this._router.navigate(['search/', this.searchForm.value.searchQuery]);
     });
   }
 
