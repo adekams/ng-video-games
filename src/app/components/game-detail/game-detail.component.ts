b22387c5d4b96794fd27e1123822d0d5ec3e0859
Adekams <kifademi@gmail.com>
2026-02-01 07:44:08 +0100

    game update

--- src/app/components/game-detail/game-detail.component.ts
+++ src/app/components/game-detail/game-detail.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-game-detail',
-    templateUrl: './game-detail.component.html',
-    styleUrls: ['./game-detail.component.scss'],
-    standalone: false
+  selector: 'app-game-detail',
+  templateUrl: './game-detail.component.html',
+  styleUrls: ['./game-detail.component.scss'],
+  standalone: false,
 })
 export class GameDetailComponent implements OnInit {
   game = JSON.parse(localStorage.getItem('game'));
+  selectedImage: string | null = null;
+
   constructor() {
     console.log('game ', this.game);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openImagePreview(image: string): void {
+    this.selectedImage = image;
   }
 
+  closeImagePreview(): void {
+    this.selectedImage = null;
+  }
 }
