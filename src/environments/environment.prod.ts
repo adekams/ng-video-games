ef4a1f90bddf59d9b3fb2a2cfca19b3d9cad1d9d
Adekams <kifademi@gmail.com>
2026-02-02 08:44:45 +0100

    chore: ignore environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   API_URL: 'https://rawg-video-games-database.p.rapidapi.com',
-  API_KEY: '0473fa5b6afe4637a1f6936b3035c014',
+
+  RAPIDAPI_KEY: '', // TODO: Add your RapidAPI key here
+  RAWG_API_KEY: '', // TODO: Add your RAWG API key here
 };
