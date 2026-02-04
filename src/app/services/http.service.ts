76ae2203bc8320568bb4245ba29384db11f84f45
Adekams <kifademi@gmail.com>
2026-02-04 19:57:51 +0100

    signals

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment as env } from '../../environments/environment';
 import { APIResponse, Game } from '../models/app-filter/app-filter';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
@@ -12,21 +13,42 @@ export class HttpService {
 
   getGames(
     order: string,
-    page: any,
+    page: number,
     search?: string,
+    pageSize = 100,
   ): Observable<APIResponse<Game>> {
-    let params = new HttpParams().set('ordering', order).set('page', page);
+    let params = new HttpParams()
+      .set('ordering', order)
+      .set('page', page.toString())
+      .set('page_size', pageSize.toString());
 
     if (search) {
-      params = new HttpParams()
-        .set('or', order)
-        .set('page', page)
-        .set('search', search);
+      params = params.set('search', search);
     }
 
-    console.log('outside type is ' + typeof page);
     return this.http.get<APIResponse<Game>>(`${env.API_URL}/games`, {
       params: params,
     });
+  }
+
+  getGamesSignal(
+    order: Signal<string>,
+    page: Signal<number>,
+    search?: Signal<string>,
+  ): Signal<APIResponse<Game> | undefined> {
+    return toSignal(
+      new Observable((observer) => {
+        const currentOrder = order();
+        const currentPage = page();
+        const currentSearch = search?.();
+
+        this.getGames(currentOrder, currentPage, currentSearch).subscribe({
+          next: (result) => observer.next(result),
+          error: (error) => observer.error(error),
+          complete: () => observer.complete(),
+        });
+      }),
+      { initialValue: undefined },
+    );
   }
 }
